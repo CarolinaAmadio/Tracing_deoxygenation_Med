ff515da473f31178fdaa3b49846c806aafd2ba41
CarolinaAmadio <33064803+CarolinaAmadio@users.noreply.github.com>
2026-03-30 14:22:07 +0200

    Move Launcher.sh into yearly_clim directory

--- 0_clim_calc/yearly_clim/Launcher.sh
+++ 0_clim_calc/yearly_clim/Launcher.sh
@@ -9,17 +9,16 @@
 #SBATCH --qos=qos_meteo
 
 cd $SLURM_SUBMIT_DIR
-. /g100_scratch/userexternal/camadio0/ARGOPY_TESTS/Climatologies_Argopy/utils/profile.inc
+. /g100_scratch/userexternal/camadio0/Tracing_deoxygenation_Med/0_clim_calc/utils/profile.inc 
 
 echo "Job started at: $(date)"
 
 source /g100_work/OGS23_PRACE_IT/COPERNICUS/py_env_3.9.18_new/bin/activate
-export PYTHONPATH=/g100_scratch/userexternal/camadio0/ARGOPY_TESTS/bit.sea/src:$PYTHONPATH
-export ONLINE_REPO=/g100_scratch/userexternal/camadio0/ARGOPY_TESTS/Climatologies_Argopy/ONLINE/
+export PYTHONPATH=/g100_scratch/userexternal/camadio0/Tracing_deoxygenation_Med/bit.sea/src:$PYTHONPATH
+export ONLINE_REPO=/g100_scratch/userexternal/camadio0/Tracing_deoxygenation_Med/ONLINE/
 
-
-
-OUTDIR=/g100_scratch/userexternal/camadio0/ARGOPY_TESTS/Climatologies_Argopy/bitsea_complement/CLIMATOLOGIES/SUPERFLOAT/
+OUTDIR=
+/g100_scratch/userexternal/camadio0/ARGOPY_TESTS/Climatologies_Argopy/bitsea_complement/CLIMATOLOGIES/SUPERFLOAT/
 
 mkdir -p $OUTDIR $OUTDIR/FILES_and_FIGS $OUTDIR/CLIMAfloat/
 
@@ -31,14 +30,14 @@ for VARNAME in O2o; do
     my_prex_or_die "python Yr_Climfloat_netcdf_Coriolis.py -o $OUT/CORIOLIS/ -v $VARNAME"
     my_prex_or_die "python compare_clima_doxy.py -i $OUT/ -o $OUT/CLIMA_FLOAT/ -v $VARNAME"
     my_prex_or_die "python clim_visualizer_html_pdf.py -i $OUT/CLIMA_FLOAT/ -v $VARNAME"
-    #python test.py -i $OUT/CLIMA_FLOAT/
+    python test.py -i $OUT/CLIMA_FLOAT/
 done
 
 
 deactivate
 source /g100/home/userexternal/camadio0/envs/py38_seaborn/bin/activate
-#for VARNAME in N3n pH N1p DIC N5s ALK O2o; do
-#    python clim_visualizer_html_pdf.py -i $OUTDIR/CLIMAfloat/ -v $VARNAME
-#done
+for VARNAME in O2o; do
+    python clim_visualizer_html_pdf.py -i $OUTDIR/CLIMAfloat/ -v $VARNAME
+done
 python test.py -i $OUT/CLIMA_FLOAT/
 
