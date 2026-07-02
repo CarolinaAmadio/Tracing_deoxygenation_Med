365d4c8a9093ca3539c457e23586779a6947f7ad
CarolinaAmadio <33064803+CarolinaAmadio@users.noreply.github.com>
2026-07-02 17:26:57 +0200

    Add coastness choice

--- 05_plot_multi_prod_timeseries/MEDBGC_INS/Launcher.sh
+++ 05_plot_multi_prod_timeseries/MEDBGC_INS/Launcher.sh
@@ -6,17 +6,25 @@
 
 source /g100_work/OGS23_PRACE_IT/COPERNICUS/py_env_3.9.18_new/bin/activate
 export PYTHONPATH=/g100_scratch/userexternal/camadio0/Tracing_deoxygenation_Med/bit.sea/src:$PYTHONPATH
+export MASKFILE="/g100_work/OGS_test2528/camadio/Neccton_hindcast_ALL_SIMULATIONS_archieve/Neccton_hindcast1999_2022/wrkdir/MASKS/meshmask.nc"
+
+
+COAST_LIST=("open_sea")
+#COAST_LIST=("everywhere" "open_sea" "coast")
 
 MIN_DEPTH=0
 MAX_DEPTH=10
-VARIABLES=(ALK pH_ins_merged DIC_merged pCO2_rec O2o)
+VARIABLES=(ALK pH_ins_merged DIC_merged pCO2_rec O2o N3n)
 
 for VAR in "${VARIABLES[@]}"; do
-    echo "Running timeseries_at_surface.py for variable: ${VAR}"
-    python timeseries_at_surface_medbgcins.py \
-        -v "${VAR}" \
-        --min-depth "${MIN_DEPTH}" \
-        --max-depth "${MAX_DEPTH}"
+    for coastness in "${COAST_LIST[@]}"; do
+        echo "Running timeseries_at_surface.py for variable: ${VAR} coastness: ${coastness}"
+        python timeseries_at_surface_medbgcins.py \
+            -v "${VAR}" \
+            --min-depth "${MIN_DEPTH}" \
+            --max-depth "${MAX_DEPTH}" \
+            --maskfile "${MASKFILE}" \
+            --coastness "${coastness}"
 
     case "${VAR}" in
         pCO2_rec)
@@ -36,7 +44,4 @@ for VAR in "${VARIABLES[@]}"; do
             ;;
     esac
 
-    #echo "Plotting variable: ${PLOT_VAR}"
-    #python ../plot_variable_timeseries.py --var "${PLOT_VAR}"
-    #echo
 done
