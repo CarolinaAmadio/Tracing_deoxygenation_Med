7eb1bea8511a1f260f1c0584da63353f1c85f770
CarolinaAmadio <33064803+CarolinaAmadio@users.noreply.github.com>
2026-07-24 19:18:16 +0200

    Bug fix

--- 05_plot_multi_prod_timeseries/MEDBGC_INS/Launcher.sh
+++ 05_plot_multi_prod_timeseries/MEDBGC_INS/Launcher.sh
@@ -5,15 +5,17 @@
 # for a list of allowed carbon variables.
 
 source /g100_work/OGS23_PRACE_IT/COPERNICUS/py_env_3.9.18_new/bin/activate
-export PYTHONPATH=/g100_scratch/userexternal/camadio0/Tracing_deoxygenation_Med/bit.sea/src:$PYTHONPATH
 export MASKFILE="/g100_work/OGS_test2528/camadio/Neccton_hindcast_ALL_SIMULATIONS_archieve/Neccton_hindcast1999_2022/wrkdir/MASKS/meshmask.nc"
+
+export PYTHONPATH=/g100_scratch/userexternal/camadio0/Tracing_deoxygenation_Med/05_plot_multi_prod_timeseries/bit.sea/src:$PYTHONPATH
+python -c "import bitsea; print(bitsea.__file__)"
 
 
 COAST_LIST=("open_sea")
 #COAST_LIST=("everywhere" "open_sea" "coast")
 
-MIN_DEPTH=0
-MAX_DEPTH=10
+MIN_DEPTH=80
+MAX_DEPTH=120
 VARIABLES=(ALK pH_ins_merged DIC_merged pCO2_rec O2o N3n)
 
 for VAR in "${VARIABLES[@]}"; do
@@ -25,6 +27,7 @@ for VAR in "${VARIABLES[@]}"; do
             --max-depth "${MAX_DEPTH}" \
             --maskfile "${MASKFILE}" \
             --coastness "${coastness}"
+    done
 
     case "${VAR}" in
         pCO2_rec)
