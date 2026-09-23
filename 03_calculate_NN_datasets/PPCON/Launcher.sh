4ed34dbb301e349a9d32d1e7c57547b7538e8694
CarolinaAmadio <33064803+CarolinaAmadio@users.noreply.github.com>
2026-09-23 17:53:48 +0200

    update file

--- 03_calculate_NN_datasets/PPCON/Launcher.sh
+++ 03_calculate_NN_datasets/PPCON/Launcher.sh
@@ -49,11 +49,9 @@ mkdir -p $ONLINE_REPO_CLUSTERING
 
 cd  ${WORKDIR}/bit.sea/src/bitsea/Float/ppcon/
 
-#my_prex_or_die "python -u clustering/clustering.py -i $ONLINE_REPO -u $ONLINE_REPO/SUPERFLOAT/Float_Index.txt -o $ONLINE_REPO_CLUSTERING"
+my_prex_or_die "python -u clustering/clustering.py -i $ONLINE_REPO -u $ONLINE_REPO/SUPERFLOAT/Float_Index.txt -o $ONLINE_REPO_CLUSTERING"
 #cd $SLURM_SUBMIT_DIR 
 my_prex_or_die "python -u make_generated_ds/generate_netcdf_netcdf4.py -t $ONLINE_REPO_CLUSTERING -m $TRAIN_DIR -p $ONLINE_REPO/PPCON"
-#/g100_scratch/userexternal/camadio0/Tracing_deoxygenation_Med_ISSUE_01/03_calculate_NN_datasets/PPCON/static-data/PPCON_TRAIN/NITRATE/2023-12-16_
-exit 0
 my_prex_or_die "python dump_index.py -i $ONLINE_REPO/PPCON -o $ONLINE_REPO/PPCON/Float_Index.txt -t ppcon_float"
 
 
